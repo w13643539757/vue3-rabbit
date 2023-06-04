2d6a0c5a96b359b200b488c95133bfcec4999d5e
wanghongbin <2945807010@qq.com>
2023-06-04 20:57:02 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 
 </script>
 
@@ -14,5 +15,11 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+<HomeProduct />
+<HomePanel title="新鲜好物" sub-title="新鲜好物 人气商品">
+
+</HomePanel>
+<HomePanel title="人气商品" sub-title="新鲜好物 人气商品">
+  
+</HomePanel>
 </template>
