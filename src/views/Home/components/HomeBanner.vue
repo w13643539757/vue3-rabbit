0a66d3862cdc48f6d0beb0b1cc5e1808990abd71
wanghongbin <2945807010@qq.com>
2023-06-04 18:38:14 +0800

    home-banner轮播图

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,3 +1,55 @@
+<script setup>
+import {getBannerAPI} from '@/apis/home';
+import { onMounted, ref } from 'vue';
+const bannerList = ref([])
+const getBanner = async () => {
+    const res = await getBannerAPI()
+bannerList.value = res.result
+}
+onMounted(() => getBanner())
+</script>
+
+
+
 <template>
-    <div></div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+
+
+<style scoped lang='scss'>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
+```
+## 2. 获取数据渲染组件
+1- 封装接口
+```javascript
+/**
+ * @description: 获取banner图
+ * @param {*}
+ * @return {*}
+ */
+import  httpInstance  from '@/utils/http'
+function getBannerAPI (){
+  return request({
+    url:'home/banner'
+  })
+}
