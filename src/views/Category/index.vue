8a8cebf83860856fc73f318986ac715000e34b7f
wanghongbin <2945807010@qq.com>
2023-06-05 17:06:55 +0800

    home轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,14 +1,23 @@
 <script setup>
-import {getCategoryAPI} from '@/apis/category'
-import {  ref, watch } from 'vue'
-import {useRoute} from 'vue-router'
-const categoryData = ref({})
-const route = useRoute()
-const getCategory = async () =>{
-  const res = await getCategoryAPI(route.params.id)
-  categoryData.value = res.result
-}
-watch(() => getCategory() ) 
+import { getCategoryAPI } from "@/apis/category";
+import { onMounted, ref, watch } from "vue";
+import { useRoute } from "vue-router";
+import { getBannerAPI } from "@/apis/home";
+const categoryData = ref({});
+const route = useRoute();
+const getCategory = async () => {
+  const res = await getCategoryAPI(route.params.id);
+  categoryData.value = res.result;
+};
+watch(() => getCategory());
+
+//获取Banner
+const bannerList = ref([]);
+const getBanner = async () => {
+  const res = await getBannerAPI();
+  bannerList.value = res.result;
+};
+onMounted(() => getBanner());
 </script>
 
 <template>
@@ -18,8 +27,16 @@ watch(() => getCategory() )
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>{{categoryData.name}}</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="" />
+          </el-carousel-item>
+        </el-carousel>
       </div>
     </div>
   </div>
@@ -48,7 +65,6 @@ watch(() => getCategory() )
       li {
         width: 168px;
         height: 160px;
-
 
         a {
           text-align: center;
@@ -102,6 +118,17 @@ watch(() => getCategory() )
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
