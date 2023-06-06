143b59d09bfeb38b412eb8932e628cbc4978d687
wanghongbin <2945807010@qq.com>
2023-06-06 21:49:25 +0800

    登录-表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -27,6 +27,18 @@ const rules = {
     },
   ],
 };
+//3.获取form实例做统一校验
+const formRef = ref(null);
+const doLogin = () => {
+  //调用实例方法
+  formRef.value.validate((valid) => {
+    //valid:所有表单都通过校验 为true
+    //以valid为判断条件 如果通过校验才执行登录逻辑
+    if (valid) {
+      //
+    }
+  });
+};
 </script>
 
 
@@ -52,6 +64,7 @@ const rules = {
         <div class="account-box">
           <div class="form">
             <el-form
+              ref="formRef"
               :model="form"
               :rules="rules"
               label-position="right"
@@ -69,7 +82,9 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin"
+                >点击登录</el-button
+              >
             </el-form>
           </div>
         </div>
