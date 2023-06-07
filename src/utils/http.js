2115652abe90fc3c30e88db28984abb4149d5841
WangHB <2945807010@qq.com>
2023-06-07 16:20:58 +0800

    登录-token拦截器携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,24 +2,33 @@
 import axios from 'axios'
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
+import { useUserStore } from '@/stores/user'
 const httpInstance = axios.create({
-    baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
-    timeout: 5000
+  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  timeout: 5000
 })
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
-    return config
-  }, e => Promise.reject(e))
-  
-  // axios响应式拦截器
-  httpInstance.interceptors.response.use(res => res.data, e => {
-//统一错误提示
-ElMessage({
-  type: 'warning',
-  message: e.response.data.message
-})
-    return Promise.reject(e)
-  })
+  //1.从pinia获取token数据
+  const userStore = useUserStore()
+  //2.按照后端要求凭借token数据
+  const token = userStore.userInfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
 
-  export default httpInstance
+  return config
+}, e => Promise.reject(e))
+
+// axios响应式拦截器
+httpInstance.interceptors.response.use(res => res.data, e => {
+  //统一错误提示
+  ElMessage({
+    type: 'warning',
+    message: e.response.data.message
+  })
+  return Promise.reject(e)
+})
+
+export default httpInstance
